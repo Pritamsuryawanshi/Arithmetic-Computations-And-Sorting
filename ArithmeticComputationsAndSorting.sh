65676669a0d1243480f71b14f8bc09c6135eebe3
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-01-30 10:11:17 +0530

    ADDED: Calculated first computation

--- ArithmeticComputationsAndSorting.sh
+++ ArithmeticComputationsAndSorting.sh
@@ -5,3 +5,6 @@ echo "Welcome to Arithmetic Computations"
 read -p "Enter the first number: " a
 read -p "Enter the second number: " b
 read -p "Enter the third number: " c
+
+result1=`echo "scale=2; $a+$b*$c" | bc ` 
+
