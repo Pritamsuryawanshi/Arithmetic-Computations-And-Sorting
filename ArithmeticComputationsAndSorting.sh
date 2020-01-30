310bede448c111fdf1ae31ffdae80d7086add320
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-01-30 10:41:06 +0530

    ADDED: Calculated fourth computation

--- ArithmeticComputationsAndSorting.sh
+++ ArithmeticComputationsAndSorting.sh
@@ -6,6 +6,7 @@ read -p "Enter the first number: " a
 read -p "Enter the second number: " b
 read -p "Enter the third number: " c
 
-result1=`echo "scale=2; $a + $b *$c" | bc `
+result1=`echo "$a + $b *$c" | bc `
 result2=`echo "scale=2; $a * $b + $c" | bc ` 
 result3=`echo "scale=2; $c + $a / $b" | bc `
+result4=`echo "scale=2; $a % $b + $c" | bc `
