72f6418930a644704edb6d04f1f94205b9415b49
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-01-30 11:22:00 +0530

    ADDED: Stored all the computations in a Dictionary

--- ArithmeticComputationsAndSorting.sh
+++ ArithmeticComputationsAndSorting.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -x
 
 echo "Welcome to Arithmetic Computations"
-
+declare -A resultsDictionary
 read -p "Enter the first number: " a
 read -p "Enter the second number: " b
 read -p "Enter the third number: " c
 
-result1=`echo "$a + $b *$c" | bc `
+result1=`echo "scale=2; $a + $b *$c" | bc `
 result2=`echo "scale=2; $a * $b + $c" | bc ` 
 result3=`echo "scale=2; $c + $a / $b" | bc `
 result4=`echo "scale=2; $a % $b + $c" | bc `
+
+resultsDictionary[result1]=$result1
+resultsDictionary[result2]=$result2
+resultsDictionary[result3]=$result3
+resultsDictionary[result4]=$result4
+
+
